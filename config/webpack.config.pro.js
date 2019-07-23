206c432bb225353dbdd922a0d2d12d3e98e00e81
albanjin <albanjin@sina.com>
2019-07-23 14:28:22 +0800

    update:按需懒加载

--- config/webpack.config.pro.js
+++ config/webpack.config.pro.js
@@ -1,40 +1,40 @@
-
 const webpack = require('webpack')
 const webpackMerge = require('webpack-merge')
 const baseConfig = require('./webpack.config.base')
-const CleanWebpackPlugin = require('clean-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const path = require('path')
+// const CleanWebpackPlugin = require('clean-webpack-plugin')
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+// const HtmlWebpackPlugin = require('html-webpack-plugin')
+// const path = require('path')
 
+module.exports = webpackMerge(baseConfig, {
+  mode: 'production',
 
+  devtool: '#source-map',
 
-module.exports = webpackMerge(baseConfig,{
-    mode: 'production',
-    
-    devtool: '#source-map',
-    optimization: {
-
-        splitChunks: {
-            automaticNameDelimiter: 'jin',
-            chunks: 'all'
-        },
-        // runtimeChunk:true
+  performance: {
+    hints: false
+  },
+  optimization: {
+    splitChunks: {
+      automaticNameDelimiter: 'jin',
+      chunks: 'all'
     },
-    module:{
-        
-    },
-    plugins:[
-        
-        // new CleanWebpackPlugin(['/dist']),
-        new webpack.DefinePlugin({
-            ROOTPATH: JSON.stringify('/ycd/app'),
-            ENV: 'true'
-        }),
-        new webpack.BannerPlugin(`@license Company: NZJR        
+  // runtimeChunk:true
+  },
+  module: {
+
+  },
+  plugins: [
+
+    // new CleanWebpackPlugin(['/dist']),
+    new webpack.DefinePlugin({
+      ROOTPATH: JSON.stringify('/ycd/app'),
+      ENV: 'true'
+    }),
+    new webpack.BannerPlugin(`@license Company: NZJR        
         @license Author: albanjin
-        (c) ${new Date().getFullYear()}-${(new Date().getMonth() / 1 + 1).toString().padStart(2,'0')}-${new Date().getDate()}`),
-        
-    ],
-    
+        (c) ${new Date().getFullYear()}-${(new Date().getMonth() / 1 + 1).toString().padStart(2,'0')}-${new Date().getDate()}`)
+
+  ]
+
 })
